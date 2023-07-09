4177db1448132942b9eb58fc0c7bf4b632afaa28
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2023-07-09 23:03:50 +0900

    implove 575 page
    - add id to each 575
    - add 575 URL to each 575 RSS item
    - add 575's okimochi by using <details> tag

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from datetime import datetime
 from git import *
+from hashlib import sha256
 from typing import List
 
 
@@ -15,6 +16,8 @@ class LineInfo:
     date: str
     line: str
     line_number: int
+    guid: str
+    okimochi: str
 
 
 def get_line_info_list(file: str) -> List[LineInfo]:
@@ -33,12 +36,22 @@ def get_line_info_list(file: str) -> List[LineInfo]:
         author = str(commit.author)
         date = str(commit.authored_datetime.strftime('%Y-%m-%d %H:%M:%S %z'))
         linenos = blame_entry.linenos
+        commit_msg_lines = commit.message.split('\n')
+        if len(commit_msg_lines) < 3:
+            okimochi = ''
+        else:
+            commit_msg_lines = commit_msg_lines[2:]
+            okimochi = '\n'.join(commit_msg_lines)
 
         for i in linenos:
             line = lines_575[i-1].rstrip()
             if bool(li_head.match(line)):
-                line = line[2:]
-                line_info = LineInfo(author, commit.hexsha, date, line, i)
+                line = line[2:]  # `- ` を除去
+                commit_hash = commit.hexsha
+                guid = sha256(("%s%s%s%s" % (line, author, date,
+                                             commit_hash)).encode()).hexdigest()
+                line_info = LineInfo(author, commit_hash,
+                                     date, line, i, guid, okimochi)
                 line_info_list.append(line_info)
 
     return line_info_list
